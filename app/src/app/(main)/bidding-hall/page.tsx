28c91cc965b2b1633465992114c9780dcbb04fca
SiavasHHamiri <siavashhamiri@gmail.com>
2025-12-19 15:09:44 +0000

    من فکر می‌کردم شاید بتونید به ابزارش رو داشته باشید و چنانچه در کل سیستم

--- app/src/app/(main)/bidding-hall/page.tsx
+++ app/src/app/(main)/bidding-hall/page.tsx
@@ -120,9 +120,9 @@ export default function BiddingHallPage() {
                                     <SelectValue placeholder="Select a project type" />
                                 </SelectTrigger>
                                 <SelectContent>
-                                    <SelectItem value="translation"><div className="flex items-center gap-2"><Languages/> Translation</div></SelectItem>
-                                    <SelectItem value="content-creation"><div className="flex items-center gap-2"><BookText/> Content Creation</div></SelectItem>
-                                    <SelectItem value="technical-writing"><div className="flex items-center gap-2"><FileText/> Technical Writing</div></SelectItem>
+                                    <SelectItem value="translation"><div className="flex items-center gap-2"><Languages className="h-4 w-4" /> Translation</div></SelectItem>
+                                    <SelectItem value="content-creation"><div className="flex items-center gap-2"><BookText className="h-4 w-4" /> Content Creation</div></SelectItem>
+                                    <SelectItem value="technical-writing"><div className="flex items-center gap-2"><FileText className="h-4 w-4" /> Technical Writing</div></SelectItem>
                                     <SelectItem value="other">Other</SelectItem>
                                 </SelectContent>
                              </Select>
